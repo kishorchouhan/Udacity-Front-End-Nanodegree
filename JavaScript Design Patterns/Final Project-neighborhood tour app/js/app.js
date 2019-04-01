00540243569836143284e63546ac23a620b3aa2e
kishorchouhan <devkishorbwr@gmail.com>
2019-04-01 16:12:35 +0530

    JavaScript Design Pattern: Final project neighborhood map CSS modified

--- JavaScript Design Patterns/Final Project-neighborhood tour app/js/app.js
+++ JavaScript Design Patterns/Final Project-neighborhood tour app/js/app.js
@@ -109,18 +109,6 @@ var Location = function(data) {
 	};
 };
 
-/*
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-  
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}*/
-
-
 function viewModel() {
     var self = this;
 
